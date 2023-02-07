dc757c022029271c8d71fe80fb5d57dfbc183765
Marek Świderek <mswiderek@eversis.com>
2023-02-07 16:44:52 +0100

    GPP-355 Configuring nginx headers for keycloak

--- geoss-nginx/docker-entrypoint.d/25-setup-csp-headers.sh
+++ geoss-nginx/docker-entrypoint.d/25-setup-csp-headers.sh
@@ -9,7 +9,8 @@ if [ -n "${CSP_DOMAINS}" ]; then
   done
 fi
 
-headers="add_header Content-Security-Policy \"default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data:${domains};\" always;"
+#headers="add_header Content-Security-Policy \"default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data:${domains};\" always;"
+headers="add_header Content-Security-Policy \"default-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-ancestors 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/\" always;"
 
 mv /etc/nginx/conf.d/utils/proxy_response_headers.conf /etc/nginx/conf.d/utils/proxy_response_headers.conf.old
 awk -v r="${headers}" '{gsub(/###CSP_HEADERS###/,r)}1' /etc/nginx/conf.d/utils/proxy_response_headers.conf.old > /etc/nginx/conf.d/utils/proxy_response_headers.conf
