5e3690a7810c981f60936c42dc6780aa7bfe93e4
Marek Świderek <mswiderek@eversis.com>
2023-02-08 14:57:25 +0100

    GPP-355 Removed response headers CSP for testing purposes

--- geoss-nginx/docker-entrypoint.d/25-setup-csp-headers.sh
+++ geoss-nginx/docker-entrypoint.d/25-setup-csp-headers.sh
@@ -1,20 +1,18 @@
-#!/bin/sh
+# #!/bin/sh
+# 
+# domains=""
+# 
+# if [ -n "${CSP_DOMAINS}" ]; then
+#   for csp_domain in $(echo ${CSP_DOMAINS} | tr "," "\n")
+#   do
+#     domains="${domains} ${csp_domain}"
+#   done
+# fi
+# 
+# headers="add_header Content-Security-Policy \"default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data:${domains};\" always;"
+# #headers="add_header Content-Security-Policy \"default-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-ancestors 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/\" always;"
+# 
+# mv /etc/nginx/conf.d/utils/proxy_response_headers.conf /etc/nginx/conf.d/utils/proxy_response_headers.conf.old
+# awk -v r="${headers}" '{gsub(/###CSP_HEADERS###/,r)}1' /etc/nginx/conf.d/utils/proxy_response_headers.conf.old > /etc/nginx/conf.d/utils/proxy_response_headers.conf
+# rm /etc/nginx/conf.d/utils/proxy_response_headers.conf.old
 
-domains=""
-
-if [ -n "${CSP_DOMAINS}" ]; then
-  for csp_domain in $(echo ${CSP_DOMAINS} | tr "," "\n")
-  do
-    domains="${domains} ${csp_domain}"
-  done
-fi
-
-headers="add_header Content-Security-Policy \"default-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data:${domains};\" always;"
-#headers="add_header Content-Security-Policy \"default-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-ancestors 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/; frame-src 'self' https://gpp.devel.esaportal.eu/ https://gpp-idp.devel.esaportal.eu/\" always;"
-
-mv /etc/nginx/conf.d/utils/proxy_response_headers.conf /etc/nginx/conf.d/utils/proxy_response_headers.conf.old
-awk -v r="${headers}" '{gsub(/###CSP_HEADERS###/,r)}1' /etc/nginx/conf.d/utils/proxy_response_headers.conf.old > /etc/nginx/conf.d/utils/proxy_response_headers.conf
-rm /etc/nginx/conf.d/utils/proxy_response_headers.conf.old
-
-echo "NEW cms.conf:"
-cat /etc/nginx/conf.d/cms.conf
