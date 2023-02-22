2712f0d8662ce10eaffe885ce0e3c49d1c16a909
Marek Świderek <mswiderek@eversis.com>
2023-02-22 14:14:23 +0100

    GPP-356 Configuring node envs for geoss-ui in docker-compose lan file

--- geoss-ui/nuxt.config.js
+++ geoss-ui/nuxt.config.js
@@ -100,7 +100,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: 'http://gpp-api.devel.esaportal.eu/',
+        baseURL: '/',
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
