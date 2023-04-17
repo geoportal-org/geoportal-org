9b58651e468cd785d57b8a941b2a09fc45d65b4a
Marek Świderek <mswiderek@eversis.com>
2023-04-17 10:51:08 +0200

    GPP-409 Disabled basic_auth for lab-uat env - conflicting with keycloak auth while behind apache rp

--- geoss-nginx/docker-entrypoint.d/24-setup-basic-auth.sh
+++ geoss-nginx/docker-entrypoint.d/24-setup-basic-auth.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ "${DEPLOY_ENV}" = "uat" ]; then
-  include_conf='include conf.d/common/block_unauthorized_users.conf;'
+# if [ "${DEPLOY_ENV}" = "uat" ]; then
+#   include_conf='include conf.d/common/block_unauthorized_users.conf;'
 
-  mv /etc/nginx/conf.d/portal.conf /etc/nginx/conf.d/portal.conf.old
-  awk -v r="${include_conf}" '{gsub(/###BASIC_AUTH###/,r)}1' /etc/nginx/conf.d/portal.conf.old > /etc/nginx/conf.d/portal.conf
-  rm /etc/nginx/conf.d/portal.conf.old
+#   mv /etc/nginx/conf.d/portal.conf /etc/nginx/conf.d/portal.conf.old
+#   awk -v r="${include_conf}" '{gsub(/###BASIC_AUTH###/,r)}1' /etc/nginx/conf.d/portal.conf.old > /etc/nginx/conf.d/portal.conf
+#   rm /etc/nginx/conf.d/portal.conf.old
 
-  mv /etc/nginx/conf.d/cms.conf /etc/nginx/conf.d/cms.conf.old
-  awk -v r="${include_conf}" '{gsub(/###BASIC_AUTH###/,r)}1' /etc/nginx/conf.d/cms.conf.old > /etc/nginx/conf.d/cms.conf
-  rm /etc/nginx/conf.d/cms.conf.old
-fi
+#   mv /etc/nginx/conf.d/cms.conf /etc/nginx/conf.d/cms.conf.old
+#   awk -v r="${include_conf}" '{gsub(/###BASIC_AUTH###/,r)}1' /etc/nginx/conf.d/cms.conf.old > /etc/nginx/conf.d/cms.conf
+#   rm /etc/nginx/conf.d/cms.conf.old
+# fi
 
 if [ "${DEPLOY_ENV}" = "prod" ]; then
   include_conf='include conf.d/common/block_unauthorized_users_cms.conf;'
