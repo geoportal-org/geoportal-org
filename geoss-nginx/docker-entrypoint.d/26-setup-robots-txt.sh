f43761d1c4ddf68cd10f7d4844c2508928567725
Marek Świderek <mswiderek@eversis.com>
2023-04-19 15:40:46 +0200

    GPP-516 Added robots.txt to all three domains

---
+++ geoss-nginx/docker-entrypoint.d/26-setup-robots-txt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "${DEPLOY_ENV}" = "uat" ]; then
+  robots_conf='location = /robots.txt {
+    add_header  Content-Type  text/plain;
+    return 200 "User-agent: *\\nDisallow: /\\n";
+  }'
+  mv /etc/nginx/conf.d/cms.conf /etc/nginx/conf.d/cms.conf.old
+  awk -v r="${upstream_conf}" '{gsub(/###ROBOTS_CONFIG###/,r)}1' /etc/nginx/conf.d/cms.conf.old > /etc/nginx/conf.d/cms.conf
+  rm /etc/nginx/conf.d/cms.conf.old
+else
+  echo "Skipping robots.txt configuration"
+fi
