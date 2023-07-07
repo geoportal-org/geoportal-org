f0b35b2a868399dbe5e60f4a000d2907a2aeb29f
Szymon Suchanowski <ssuchanowski@eversis.lab>
2023-07-07 15:30:45 +0200

    GPP-474 - hide tutorial tags in menu

--- geoss-admin/src/data/navData.ts
+++ geoss-admin/src/data/navData.ts
@@ -44,11 +44,13 @@ const navSettingsData: NavItem[] = [
         href: pagesRoutes.defaultLayer,
         Icon: LayerIcon,
     },
+    // hidden tutorial tags in menu
+    /*
     {
         titleId: "nav.settings.section.tutorial",
         href: pagesRoutes.tutorialTags,
         Icon: TutorialIcon,
-    },
+    },*/
 ];
 
 const navContentsData: NavItem[] = [
