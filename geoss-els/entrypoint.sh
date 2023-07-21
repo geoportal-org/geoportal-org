2c3af89407a6558ee4b65718318b7a1e1ccf2c24
Marek Świderek <mswiderek@eversis.com>
2023-07-21 13:06:16 +0200

    GPP-635 ELS Dockerfile entrypoint and cmd configuration

--- geoss-els/entrypoint.sh
+++ geoss-els/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "ENTRYPOINT: Creating indexes ..."
+echo "ENTRYPOINT: Starting ealsticsearch..."
+/bin/tini -- /usr/local/bin/docker-entrypoint.sh eswrapper&
 
 until curl -sS "http://localhost:9200/_cat/health?h=status" | grep -q "green\|yellow"; do
         sleep 1
 done
+
+echo "ENTRYPOINT: Creating indexes ..."
 
 head -n-1 /tmp/geoss-cr-settings.json > /tmp/geoss-cr.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr.json; tail -n+2 /tmp/geoss-cr-mappings.json >> /tmp/geoss-cr.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr.json
 head -n-1 /tmp/geoss-cr-extensions-settings.json > /tmp/geoss-cr-extensions.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr-extensions.json; tail -n+2 /tmp/geoss-cr-extensions-mappings.json >> /tmp/geoss-cr-extensions.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr-extensions.json
