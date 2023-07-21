ff4f2ffbed3e36c38e336976684ab2812c9919fa
Marek Świderek <mswiderek@eversis.com>
2023-07-21 15:12:57 +0200

    GPP-635 ELS Dockerfile entrypoint and cmd configuration - separated script for creating index from entrypoint script

--- geoss-els/entrypoint.sh
+++ geoss-els/entrypoint.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-echo "ENTRYPOINT: Starting ealsticsearch..."
-
-until curl -sS "http://localhost:9200/_cat/health?h=status" | grep -q "green\|yellow"; do
-        sleep 1
-done
-
-echo "ENTRYPOINT: Creating indexes ..."
-
-head -n-1 /tmp/geoss-cr-settings.json > /tmp/geoss-cr.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr.json; tail -n+2 /tmp/geoss-cr-mappings.json >> /tmp/geoss-cr.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr.json
-head -n-1 /tmp/geoss-cr-extensions-settings.json > /tmp/geoss-cr-extensions.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr-extensions.json; tail -n+2 /tmp/geoss-cr-extensions-mappings.json >> /tmp/geoss-cr-extensions.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr-extensions.json
-head -n-1 /tmp/geoss-recommendation-settings.json > /tmp/geoss-recommendation.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-recommendation.json; tail -n+2 /tmp/geoss-recommendation-mappings.json >> /tmp/geoss-recommendation.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-recommendation.json
-
-curl -XPUT 'http://localhost:9200/geoss-cr' -H "Content-Type: application/json" -d @/tmp/geoss-cr.json
-curl -XPUT 'http://localhost:9200/geoss-cr-extensions' -H "Content-Type: application/json" -d @/tmp/geoss-cr-extensions.json
-curl -XPUT 'http://localhost:9200/geoss-recommendation' -H "Content-Type: application/json" -d @/tmp/geoss-recommendation.json
-
-echo "ENTRYPOINT: Indexes created"
+/create_indexes.sh&
+/bin/tini -- /usr/local/bin/docker-entrypoint.sh $1
