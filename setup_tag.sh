f80666fba9e93b57d2ab183b3069c085fc6fe71f
kgolab <kgolab@eversis.com>
2023-08-01 12:54:04 +0200

    GPP-629 Build geoss-search docker.

--- setup_tag.sh
+++ setup_tag.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-# Set default value
-export DOCKER_IMAGE_TAG=latest
 search_for="release-"
 
-if [ "1" -eq "$(echo ${CI_COMMIT_REF_NAME} | grep -c $search_for)" ]
+if [ "develop" = "$CI_COMMIT_REF_NAME" ]
+then
+  export DOCKER_IMAGE_TAG=latest
+elif [ "uat" = "$CI_COMMIT_REF_NAME" ]
+then
+  export DOCKER_IMAGE_TAG=$CI_COMMIT_REF_NAME-$CI_COMMIT_SHORT_SHA
+elif [ "1" -eq "$(echo "${CI_COMMIT_REF_NAME}" | grep -c $search_for)" ]
 then
   export DOCKER_IMAGE_TAG=${CI_COMMIT_REF_NAME##*-}
+else
+  # Set default value
+  export DOCKER_IMAGE_TAG=$CI_COMMIT_SHORT_SHA
 fi
