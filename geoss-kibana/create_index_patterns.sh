38aaad3a21d458caabc9b0b679359459c73edb30
Marek Świderek <mswiderek@eversis.com>
2023-08-03 17:14:18 +0200

    GPP-492 Added index patterns auto-creation to kibana - fixing scripts order

---
+++ geoss-kibana/create_index_patterns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+until curl -sS "http://localhost:5601/status" -I | grep -q "kbn-name"; do
+        sleep 1
+done
+
+echo "ENTRYPOINT: Creating index-patterns"
+
+for filename in /etc/kibana-data/*.json; do
+	index_pattern_name=$(basename ${filename::-5})
+	echo "ENTRYPOINT: Creating index-pattern: ${index_pattern_name}..."
+
+	curl -X POST \
+    "localhost:5601/api/saved_objects/index-pattern/${index_pattern_name}" \
+    --header "kbn-xsrf: true" \
+    --header "Content-Type: application/json" \
+    --data @${filename}
+done
+
+echo "ENTRYPOINT: Creating index-patterns finished. Exiting."
--- geoss-kibana/entrypoint.sh
+++ geoss-kibana/entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-echo "ENTRYPOINT: Creating index-patterns"
-
-for filename in /etc/kibana-data/*.json; do
-	index_pattern_name=$(basename ${filename::-5})
-	echo "ENTRYPOINT: Creating index-pattern: ${index_pattern_name}..."
-
-	curl -X POST \
-    "localhost:5601/api/saved_objects/index-pattern/${index_pattern_name}" \
-    --header "kbn-xsrf: true" \
-    --header "Content-Type: application/json" \
-    --data @${filename}
-done
-
-echo "ENTRYPOINT: Creating index-patterns finished."
+/create_index_patterns.sh&
 /opt/bitnami/scripts/kibana/entrypoint.sh $1
