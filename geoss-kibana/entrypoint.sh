850f3bb3fd55dd316799f0c844c57a99aff2225c
Marek Świderek <mswiderek@eversis.com>
2023-08-03 13:56:48 +0200

    GPP-492 Added index patterns auto-creation to kibana

---
+++ geoss-kibana/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "ENTRYPOINT: Creating index-patterns"
+
+for filename in /etc/kibana-data/*.json; do
+	index_pattern_name=$(basename ${filename::-5})
+	echo "ENTRYPOINT: Creating index-pattern: ${index_pattern_name}..."
+
+	curl -X POST \
+    "localhost:5601/api/saved_objects/index-pattern/${index_pattern_name}" \
+    --header "kbn-xsrf: true" \
+    --header "Content-Type: application/json" \
+    --data @${filename}
+done
+
+echo "ENTRYPOINT: Creating index-patterns finished. Exiting."
