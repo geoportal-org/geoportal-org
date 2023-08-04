688147b84a980cbd4957fb628b5ddaa3ba42ea8b
kgolab <kgolab@eversis.com>
2023-08-04 17:08:19 +0200

    GPP-627 Update elasticsearch version to 8.9.0 with api compatibility mode of rest high level client.

--- geoss-search/src/main/java/com/eversis/esa/geoss/curatedrelationships/search/config/datasource/ElasticsearchConfig.java
+++ geoss-search/src/main/java/com/eversis/esa/geoss/curatedrelationships/search/config/datasource/ElasticsearchConfig.java
@@ -1,13 +1,17 @@
 package com.eversis.esa.geoss.curatedrelationships.search.config.datasource;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RestHighLevelClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * The type Elasticsearch config.
@@ -22,18 +26,46 @@ public class ElasticsearchConfig {
     @Value("${datasource.elasticsearch.port}")
     private int elasticsearchPort;
 
+    @Value("${datasource.elasticsearch.compatibilityMode}")
+    private boolean elasticsearchCompatibilityMode;
+
+    /**
+     * Rest client.
+     *
+     * @return the rest client
+     */
+    @Bean(destroyMethod = "close")
+    RestClient restClient() {
+        log.info("Configuring Elasticsearch RestClient host: {}, port: {}",
+                elasticsearchHost, elasticsearchPort);
+        return RestClient.builder(new HttpHost(elasticsearchHost, elasticsearchPort)).build();
+    }
+
     /**
      * Elasticsearch client rest high level client.
      *
+     * @param restClient the rest client
      * @return the rest high level client
      */
-    @Primary
     @Bean(destroyMethod = "close")
-    public RestHighLevelClient elasticsearchClient() {
-        log.info("Configuring Elasticsearch RestHighLevelClient host: {}, port: {}", elasticsearchHost,
-                elasticsearchPort);
-        return new RestHighLevelClient(
-                RestClient.builder(new HttpHost(elasticsearchHost, elasticsearchPort)));
+    public RestHighLevelClient restHighLevelClient(RestClient restClient) {
+        return new RestHighLevelClientBuilder(restClient)
+                .setApiCompatibilityMode(elasticsearchCompatibilityMode)
+                .build();
     }
 
+    /**
+     * Elasticsearch client.
+     *
+     * @param restClient the rest client
+     * @return the elasticsearch client
+     */
+    @Bean
+    public ElasticsearchClient elasticsearchClient(RestClient restClient) {
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient,
+                new JacksonJsonpMapper()
+        );
+        return new ElasticsearchClient(transport);
+    }
 }
