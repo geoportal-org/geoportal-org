5c8e31f366d7d01aa8ddf83deaf6685b513b1b32
kgolab <kgolab@eversis.com>
2023-08-08 09:39:53 +0200

    GPP-635 Update geoss indices

--- geoss-els/create_indexes.sh
+++ geoss-els/create_indexes.sh
@@ -5,12 +5,9 @@ done
 
 echo "ENTRYPOINT: Creating indexes ..."
 
-head -n-1 /tmp/geoss-cr-settings.json > /tmp/geoss-cr.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr.json; tail -n+2 /tmp/geoss-cr-mappings.json >> /tmp/geoss-cr.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr.json
-head -n-1 /tmp/geoss-cr-extensions-settings.json > /tmp/geoss-cr-extensions.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-cr-extensions.json; tail -n+2 /tmp/geoss-cr-extensions-mappings.json >> /tmp/geoss-cr-extensions.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-cr-extensions.json
-head -n-1 /tmp/geoss-recommendation-settings.json > /tmp/geoss-recommendation.json; sed -i '$ s/$/%SEPARATOR%/' /tmp/geoss-recommendation.json; tail -n+2 /tmp/geoss-recommendation-mappings.json >> /tmp/geoss-recommendation.json; sed -i 's/%SEPARATOR%/,/' /tmp/geoss-recommendation.json
-
-curl -XPUT 'http://localhost:9200/geoss-cr' -H "Content-Type: application/json" -d @/tmp/geoss-cr.json
-curl -XPUT 'http://localhost:9200/geoss-cr-extensions' -H "Content-Type: application/json" -d @/tmp/geoss-cr-extensions.json
-curl -XPUT 'http://localhost:9200/geoss-recommendation' -H "Content-Type: application/json" -d @/tmp/geoss-recommendation.json
+curl -XPUT 'http://localhost:9200/geoss-cr' -H 'Content-Type: application/json' -d @/indices/geoss-cr-index.json
+curl -XPUT 'http://localhost:9200/geoss-cr-extensions' -H 'Content-Type: application/json' -d @/indices/geoss-cr-extensions-index.json
+curl -XPUT 'http://localhost:9200/geoss-recommendation' -H 'Content-Type: application/json' -d @/indices/geoss-recommendation-index.json
+curl -XPUT 'http://localhost:9200/thesaurus-vocabulary' -H 'Content-Type: application/json' -d @/indices/thesaurus-vocabulary-index.json
 
 echo "ENTRYPOINT: Indexes created"
