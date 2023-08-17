a7dcca9856238149c227149c0450adc321221fef
Marek Świderek <mswiderek@eversis.com>
2023-08-17 09:33:59 +0200

    GPP-643 Configuring kibana/els oidc - entrypoint fixes

--- geoss-els/entrypoint.sh
+++ geoss-els/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 /create_indexes.sh&
-
-echo "Adding keystore value"
-echo -n "xTUIINx6U7qe6lcTcrRUdgYqVKo08dXs" | bin/elasticsearch-keystore add --stdin xpack.security.authc.realms.oidc.geoss.rp.client_secret
-bin/elasticsearch-keystore show xpack.security.authc.realms.oidc.geoss.rp.client_secret
-echo "Added keystore value"
-
-/bin/tini -- /usr/local/bin/docker-entrypoint.sh $1
+/opt/bitnami/scripts/elasticsearch/entrypoint.sh $1
