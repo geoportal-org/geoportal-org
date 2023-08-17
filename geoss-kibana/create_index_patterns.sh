63d0026a5402d3882491bbebe0cecd27741a40b2
Marek Świderek <mswiderek@eversis.com>
2023-08-17 12:20:15 +0200

    GPP-643 Reverting els stack changes - no oidc

--- geoss-kibana/create_index_patterns.sh
+++ geoss-kibana/create_index_patterns.sh
@@ -11,10 +11,10 @@ done
 echo "ENTRYPOINT: Creating index-patterns"
 
 for filename in /etc/kibana-data/*.json; do
-	index_pattern_name=$(basename ${filename::-5})
-	echo "ENTRYPOINT: Creating index-pattern: ${index_pattern_name}..."
+        index_pattern_name=$(basename ${filename::-5})
+        echo "ENTRYPOINT: Creating index-pattern: ${index_pattern_name}..."
 
-	curl -X POST \
+        curl -X POST \
     "localhost:5601/api/saved_objects/index-pattern/${index_pattern_name}" \
     --header "kbn-xsrf: true" \
     --header "Content-Type: application/json" \
