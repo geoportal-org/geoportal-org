ae3bcd2101f993c1df18d99f9dce45978c98aa07
mmorawski <mmorawski@eversis.com>
2023-08-18 15:07:45 +0200

    GPP-605 custom endpoint repository methods

--- geoss-curated/resources/src/main/java/com/eversis/esa/geoss/curated/resources/controller/EndpointController.java
+++ geoss-curated/resources/src/main/java/com/eversis/esa/geoss/curated/resources/controller/EndpointController.java
@@ -1,15 +1,19 @@
 package com.eversis.esa.geoss.curated.resources.controller;
 
 import java.util.List;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import com.eversis.esa.geoss.curated.resources.domain.Endpoint;
 import com.eversis.esa.geoss.curated.resources.repository.EndpointRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -43,7 +47,33 @@ public class EndpointController {
     @GetMapping
     public List<Endpoint> getEndpointValues() {
         log.info("Getting endpoint values...");
-        return endpointRepository.findByIsCustomOrderByUrl(0);
+        return endpointRepository.findByIsCustomOrderByUrl(0, PageRequest.of(0, 10));
+    }
+
+    /**
+     * Gets endpoint url type values.
+     *
+     * @return the endpoint url type values
+     */
+    @PreAuthorize("hasAnyRole('RESOURCE_WRITER', 'ADMIN')")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/urlTypes")
+    public List<String> getEndpointUrlTypeValues() {
+        log.info("Getting endpoint url type values...");
+        return endpointRepository.findEndpointUrlTypeValues();
+    }
+
+    /**
+     * Find endpoints list.
+     *
+     * @param query the query
+     * @return the list
+     */
+    @PreAuthorize("hasAnyRole('RESOURCE_WRITER', 'ADMIN')")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/find")
+    public List<Endpoint> findEndpoints(@RequestParam("query") @NotNull @Size(min = 2) String query) {
+        return endpointRepository.findEndpoints(query, PageRequest.of(0, 10));
     }
 
 }
--- geoss-curated/resources/src/main/java/com/eversis/esa/geoss/curated/resources/repository/EndpointRepository.java
+++ geoss-curated/resources/src/main/java/com/eversis/esa/geoss/curated/resources/repository/EndpointRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import com.eversis.esa.geoss.curated.resources.domain.Endpoint;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
@@ -26,8 +28,27 @@ public interface EndpointRepository extends JpaRepository<Endpoint, Long> {
      * Find by is custom order by url list.
      *
      * @param isCustom the is custom
+     * @param pageable the pageable
      * @return the list
      */
-    List<Endpoint> findByIsCustomOrderByUrl(int isCustom);
+    List<Endpoint> findByIsCustomOrderByUrl(int isCustom, Pageable pageable);
+
+    /**
+     * Find endpoint url type values list.
+     *
+     * @return the list
+     */
+    @Query("SELECT DISTINCT e.urlType FROM Endpoint e")
+    List<String> findEndpointUrlTypeValues();
+
+    /**
+     * Find endpoints list.
+     *
+     * @param query the query
+     * @param pageable the pageable
+     * @return the list
+     */
+    @Query("SELECT e FROM Endpoint e WHERE e.url like %:query%")
+    List<Endpoint> findEndpoints(String query, Pageable pageable);
 
 }
