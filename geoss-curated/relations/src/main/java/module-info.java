097f7258da1344cdf0311c6a185f4ba7c01594f0
mmorawski <mmorawski@eversis.com>
2023-09-26 14:09:38 +0200

    GPP-598 missing module-info

---
+++ geoss-curated/relations/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module geoss.curated.dashboards {
+    exports com.eversis.esa.geoss.curated.relations.configuration to spring.beans, spring.boot, spring.context;
+
+    requires static lombok;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires jakarta.persistence;
+    requires spring.data.jpa;
+    requires org.hibernate.orm.core;
+    requires com.fasterxml.jackson.annotation;
+    requires geoss.curated.common;
+    requires spring.data.commons;
+    requires spring.data.rest.webmvc;
+    requires spring.web;
+    requires io.swagger.v3.oas.annotations;
+    requires spring.security.core;
+    requires jakarta.validation;
+    requires spring.tx;
+    requires org.apache.logging.log4j;
+}
---
+++ geoss-curated/resources/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+module geoss.curated.resources {
+    exports com.eversis.esa.geoss.curated.resources.configuration to spring.beans, spring.boot, spring.context;
+    exports com.eversis.esa.geoss.curated.resources.domain;
+    exports com.eversis.esa.geoss.curated.resources.model;
+
+    requires static lombok;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires jakarta.persistence;
+    requires spring.data.jpa;
+    requires org.hibernate.orm.core;
+    requires com.fasterxml.jackson.annotation;
+    requires geoss.curated.common;
+    requires spring.data.commons;
+    requires org.springdoc.openapi.common;
+    requires io.swagger.v3.oas.models;
+    requires spring.data.rest.webmvc;
+    requires spring.web;
+    requires io.swagger.v3.oas.annotations;
+    requires spring.security.core;
+    requires jakarta.validation;
+    requires spring.tx;
+    requires org.apache.logging.log4j;
+}
