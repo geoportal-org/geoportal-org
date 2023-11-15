af38bdcd4416d66018b55c5d85ef243ea4611f0e
Marek Świderek <mswiderek@eversis.com>
2023-11-15 13:44:20 +0100

    GPP-723 Example bootstrap php script

--- geoss-matomo/conf/bootstrap.php
+++ geoss-matomo/conf/bootstrap.php
@@ -1,18 +1,5 @@
 <?php
-
-use Piwik\Db;
-use Piwik\Common;
-use \Exception;
-
-try {
-	$sql = "CREATE TABLE " . Common::prefixTable('mynewtable') . " (
-	            mykey VARCHAR( 10 ) NOT NULL ,
-	            mydata VARCHAR( 100 ) NOT NULL ,
-	            PRIMARY KEY ( mykey )
-	        )  DEFAULT CHARSET=utf8 ";
-	Db::exec($sql);
-	} catch (Exception $e) {
-	// ignore error if table already exists (1050 code is for 'table already exists')
-	if (!Db::get()->isErrNo($e, '1050')) {
-	    throw $e;
-	}
+// Online PHP compiler to run PHP program online
+// Print "Hello World!" message
+echo "Hello World!";
+?>
