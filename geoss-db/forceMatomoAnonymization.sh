414a2d8c33f2dd38533a10301a2c9d38ef57eda5
Marek Świderek <mswiderek@eversis.com>
2023-11-21 15:54:49 +0100

    GPP-723 Force->enable matomo IP anonymization on db init. Unlocked option to disable anonymization in matomo admin panel

--- geoss-db/forceMatomoAnonymization.sh
+++ geoss-db/forceMatomoAnonymization.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 numoftables=0
-if [ -n "$MATOMO_DATABASE_DBNAME" ] && [ -n "$MATOMO_DATABASE_USERNAME" ] && [ -n "$MATOMO_DATABASE_PASSWORD" ]; then
-    while [ "$numoftables" -lt 30 ]
-    do
-        sleep 60
-        numoftables=`mysql -s -N -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" -e "select count(table_name) from information_schema.tables where table_schema='matomodb';"`
-    done;
-fi;
 
-mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "source /opt/force_matomo_anonymization.sql"
-mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=1 where option_name='PrivacyManager.ipAnonymizerEnabled';"
-echo "Created matomo triggers - exiting"
+while [ "$numoftables" -lt 30 ]
+do
+    sleep 60
+    numoftables=`mysql -s -N -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" -e "select count(table_name) from information_schema.tables where table_schema='matomodb';"`
+done;
+
+
+mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=1 where option_name IN ('PrivacyManager.ipAnonymizerEnabled', 'PrivacyManager.useAnonymizedIpForVisitEnrichment');"
+mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=2 where option_name='PrivacyManager.ipAddressMaskLength';"
+mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=0 where option_name IN ('PrivacyManager.anonymizeUserId', 'PrivacyManager.anonymizeOrderId', 'PrivacyManager.forceCookielessTracking');"
+
+echo "Enabled matomo IP anonymization."
