f799d77f3319a8d74aa9ac9e70d460626dc582eb
Marek Świderek <mswiderek@eversis.com>
2023-11-21 17:14:42 +0100

    GPP-723 Added extra db rows for default matomo IP anonymization setting

--- geoss-db/enableMatomoAnonymization.sh
+++ geoss-db/enableMatomoAnonymization.sh
@@ -13,4 +13,6 @@ mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e
 mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=2 where option_name='PrivacyManager.ipAddressMaskLength';"
 mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "update matomo_option set option_value=0 where option_name IN ('PrivacyManager.anonymizeUserId', 'PrivacyManager.anonymizeOrderId', 'PrivacyManager.forceCookielessTracking');"
 
+mysql -h localhost -u root -p"$MARIADB_ROOT_PASSWORD" $MATOMO_DATABASE_DBNAME -e "insert ignore into matomo_option (option_name, option_value, autoload) values ('PrivacyManager.ipAddressMaskLength', '2', '0'), ('PrivacyManager.useAnonymizedIpForVisitEnrichment', '1', '0'), ('PrivacyManager.anonymizeReferrer', '', '0'), ('PrivacyManager.anonymizeUserId', '0', '0'), ('PrivacyManager.anonymizeOrderId', '0', '0'), ('PrivacyManager.forceCookielessTracking', '0', '0');"
+
 echo "Enabled matomo IP anonymization."
