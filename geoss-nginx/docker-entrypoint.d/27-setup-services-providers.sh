19a91c23a8ffd1dc946825d4ab673eb811a0448a
Marek Świderek <mswiderek@eversis.com>
2024-01-25 10:17:13 +0100

    GPP-480 Parametrized DAB cache configuration

---
+++ geoss-nginx/docker-entrypoint.d/27-setup-services-providers.sh
@@ -0,0 +1,12 @@
+if [ -n "${UI_SERVICES_PROVIDERS}" ]; then
+
+  mv /etc/nginx/conf.d/cms_conf/location_services_providers.conf /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old
+  awk -v r="${UI_SERVICES_PROVIDERS}" '{gsub(/###SERVICES_PROVIDERS_SOURCE###/,r)}1' /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old > /etc/nginx/conf.d/cms_conf/location_services_providers.conf
+  rm /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old
+
+  mv /etc/nginx/conf.d/gpp.conf /etc/nginx/conf.d/gpp.conf.old
+  awk -v r="include conf.d/cms_conf/location_services_providers.conf;" '{gsub(/###SERVICES_PROVIDERS_LOCATION###/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
+  rm /etc/nginx/conf.d/gpp.conf.old
+else
+  echo "UI_SERVICES_PROVIDERS env variable is not set"
+fi
