e49e9be862c7c20da89562c83c08648d2d802324
Marek Świderek <mswiderek@eversis.com>
2024-02-23 15:10:53 +0100

    GPP-525 Changed service_providers_location in nginx entrypoint

--- geoss-nginx/docker-entrypoint.d/27-setup-services-providers.sh
+++ geoss-nginx/docker-entrypoint.d/27-setup-services-providers.sh
@@ -5,7 +5,7 @@ if [ -n "${UI_SERVICES_PROVIDERS}" ]; then
   rm /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old
 
   mv /etc/nginx/conf.d/gpp.conf /etc/nginx/conf.d/gpp.conf.old
-  awk -v r="include cms_conf/location_services_providers.conf;" '{gsub(/###SERVICES_PROVIDERS_LOCATION###/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
+  awk -v r="include conf.d/cms_conf/location_services_providers.conf;" '{gsub(/###SERVICES_PROVIDERS_LOCATION###/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
   rm /etc/nginx/conf.d/gpp.conf.old
 else
   echo "UI_SERVICES_PROVIDERS env variable is not set"
