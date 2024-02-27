72623918b1bc2f6ff17fe1322a5707028c3ce541
Marek Świderek <mswiderek@eversis.com>
2024-02-27 10:23:21 +0100

    GPP-480 Added services-providers location inside main gpp config file

--- geoss-nginx/docker-entrypoint.d/27-setup-services-providers.sh
+++ geoss-nginx/docker-entrypoint.d/27-setup-services-providers.sh
@@ -1,11 +1,11 @@
 if [ -n "${UI_SERVICES_PROVIDERS}" ]; then
 
-  mv /etc/nginx/conf.d/cms_conf/location_services_providers.conf /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old
-  awk -v r="${UI_SERVICES_PROVIDERS}" '{gsub(/###SERVICES_PROVIDERS_SOURCE###/,r)}1' /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old > /etc/nginx/conf.d/cms_conf/location_services_providers.conf
-  rm /etc/nginx/conf.d/cms_conf/location_services_providers.conf.old
+  mv /etc/nginx/conf.d/gpp.conf /etc/nginx/conf.d/gpp.conf.old
+  awk -v r="" '{gsub(/#SP#/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
+  rm /etc/nginx/conf.d/gpp.conf.old
 
   mv /etc/nginx/conf.d/gpp.conf /etc/nginx/conf.d/gpp.conf.old
-  awk -v r="include conf.d/cms_conf/location_services_providers.conf;" '{gsub(/###SERVICES_PROVIDERS_LOCATION###/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
+  awk -v r="${UI_SERVICES_PROVIDERS}" '{gsub(/###SERVICES_PROVIDERS_SOURCE###/,r)}1' /etc/nginx/conf.d/gpp.conf.old > /etc/nginx/conf.d/gpp.conf
   rm /etc/nginx/conf.d/gpp.conf.old
 else
   echo "UI_SERVICES_PROVIDERS env variable is not set"
