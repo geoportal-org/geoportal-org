ab1c58cf183c67b7166d9a215111e69ad19712c8
Maciej Siola <msiola@eversis.com>
2024-10-11 11:25:11 +0200

    GPP-000 - DAM-158 - Add matomo chart

--- charts/install.sh
+++ charts/install.sh
@@ -17,3 +17,8 @@ envsubst < geoss-kibana/values.yaml.template > geoss-kibana/values.yaml
 helm -n $K8S_NAMESPACE upgrade --install \
     --debug $RESOURCE_NAME_PREFIX-kibana geoss-kibana | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
 
+printf "\n\n ${Green}Deploy GEOSS-matomo ...\n\n${NC}"
+envsubst < geoss-matomo/values.yaml.template > geoss-matomo/values.yaml
+helm -n $K8S_NAMESPACE upgrade --install \
+    --debug $RESOURCE_NAME_PREFIX-matomo geoss-matomo | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
+
