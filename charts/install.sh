24e8e21794db49f40eb778cc421a183ab1b220eb
Maciej Siola <msiola@eversis.com>
2024-10-11 11:25:11 +0200

    GPP-000 - DAM-154 - Add db chart

--- charts/install.sh
+++ charts/install.sh
@@ -5,12 +5,10 @@ NC='\033[0m' # No Color
 
 source .env
 
-cp *.crt geoss-nginx/.
-cp *.key geoss-nginx/.
-printf "\n\n ${Green}Deploy GEOSS-nginx ...\n\n${NC}"
-envsubst < geoss-nginx/values.yaml.template > geoss-nginx/values.yaml
+printf "\n\n ${Green}Deploy GEOSS-db ...\n\n${NC}"
+envsubst < geoss-db/values.yaml.template > geoss-db/values.yaml
 helm -n $K8S_NAMESPACE upgrade --install \
-    --debug $RESOURCE_NAME_PREFIX-nginx geoss-nginx | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
+    --debug $RESOURCE_NAME_PREFIX-db geoss-db | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
 
 printf "\n\n ${Green}Deploy GEOSS-kibana ...\n\n${NC}"
 envsubst < geoss-kibana/values.yaml.template > geoss-kibana/values.yaml
@@ -22,3 +20,9 @@ envsubst < geoss-matomo/values.yaml.template > geoss-matomo/values.yaml
 helm -n $K8S_NAMESPACE upgrade --install \
     --debug $RESOURCE_NAME_PREFIX-matomo geoss-matomo | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
 
+cp *.crt geoss-nginx/.
+cp *.key geoss-nginx/.
+printf "\n\n ${Green}Deploy GEOSS-nginx ...\n\n${NC}"
+envsubst < geoss-nginx/values.yaml.template > geoss-nginx/values.yaml
+helm -n $K8S_NAMESPACE upgrade --install \
+    --debug $RESOURCE_NAME_PREFIX-nginx geoss-nginx | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
