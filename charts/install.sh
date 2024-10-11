d3b9e91e590f04f9d19362b90d2e25d15c1a767e
Maciej Siola <msiola@eversis.com>
2024-10-11 11:25:11 +0200

    GPP-000 - DAM-174 - Add basic CI/CD

---
+++ charts/install.sh
@@ -0,0 +1,18 @@
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+NC='\033[0m' # No Color
+
+
+printf "\n\n ${Green}Deploy GEOSS-nginx ...\n\n${NC}"
+envsubst < geoss-nginx/values.yaml.template > geoss-nginx/values.yaml
+helm -n $K8S_NAMESPACE upgrade --install \
+    --debug $RESOURCE_NAME_PREFIX-nginx geoss-nginx \
+    --wait --timeout 15m  | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
+
+#printf "\n\n ${Green}Deploy Nginx ...\n\n${NC}"
+#envsubst < nginx/values.yaml.template > nginx/values.yaml
+#helm -n $K8S_NAMESPACE upgrade --install \
+#    --debug $RESOURCE_NAME_PREFIX-nginx nginx \
+#    --wait --timeout 15m  | grep -E "(Happy\ Helming|NAME\: |LAST DEPLOYED\: |NAMESPACE\: |STATUS\: |REVISION\: | TEST SUITE\: )"  || true
+
